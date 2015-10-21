89580889869cdcd9fc301c56ee01fd8f627d0412
Scholer <scholer_l@live.com>
2015-10-21 09:29:49 +0800

    base compiler

---
+++ BladeCompiler.php
@@ -0,0 +1,486 @@
+<?php
+
+namespace Xiaoler\Blade;
+
+class BladeCompiler
+{
+    /**
+     * The file currently being compiled.
+     *
+     * @var string
+     */
+    protected $path;
+
+    /**
+     * All of the available compiler functions.
+     *
+     * @var array
+     */
+    protected $compilers = [
+        'Statements',
+        'Comments',
+        'Echos',
+    ];
+
+    /**
+     * Array of opening and closing tags for raw echos.
+     *
+     * @var array
+     */
+    protected $rawTags = ['{!!', '!!}'];
+
+    /**
+     * Array of opening and closing tags for regular echos.
+     *
+     * @var array
+     */
+    protected $contentTags = ['{{', '}}'];
+
+    /**
+     * Array of opening and closing tags for escaped echos.
+     *
+     * @var array
+     */
+    protected $escapedTags = ['{{{', '}}}'];
+
+    /**
+     * The "regular" / legacy echo string format.
+     *
+     * @var string
+     */
+    protected $echoFormat = 'e(%s)';
+
+    /**
+     * Compile the given Blade template contents.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function compileString($value)
+    {
+        $result = '';
+
+        // Here we will loop through all of the tokens returned by the Zend lexer and
+        // parse each one into the corresponding valid PHP. We will then have this
+        // template as the correctly rendered PHP that can be rendered natively.
+        foreach (token_get_all($value) as $token) {
+            $result .= is_array($token) ? $this->parseToken($token) : $token;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Parse the tokens from the template.
+     *
+     * @param  array  $token
+     * @return string
+     */
+    protected function parseToken($token)
+    {
+        list($id, $content) = $token;
+
+        if ($id == T_INLINE_HTML) {
+            foreach ($this->compilers as $type) {
+                $content = $this->{"compile{$type}"}($content);
+            }
+        }
+
+        return $content;
+    }
+
+    /**
+     * Compile Blade comments into valid PHP.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function compileComments($value)
+    {
+        $pattern = sprintf('/%s--((.|\s)*?)--%s/', $this->contentTags[0], $this->contentTags[1]);
+
+        return preg_replace($pattern, '<?php /*$1*/ ?>', $value);
+    }
+
+    /**
+     * Compile Blade echos into valid PHP.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function compileEchos($value)
+    {
+        foreach ($this->getEchoMethods() as $method => $length) {
+            $value = $this->$method($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Get the echo methods in the proper order for compilation.
+     *
+     * @return array
+     */
+    protected function getEchoMethods()
+    {
+        $methods = [
+            'compileRawEchos' => strlen(stripcslashes($this->rawTags[0])),
+            'compileEscapedEchos' => strlen(stripcslashes($this->escapedTags[0])),
+            'compileRegularEchos' => strlen(stripcslashes($this->contentTags[0])),
+        ];
+
+        uksort($methods, function ($method1, $method2) use ($methods) {
+            // Ensure the longest tags are processed first
+            if ($methods[$method1] > $methods[$method2]) {
+                return -1;
+            }
+            if ($methods[$method1] < $methods[$method2]) {
+                return 1;
+            }
+
+            // Otherwise give preference to raw tags (assuming they've overridden)
+            if ($method1 === 'compileRawEchos') {
+                return -1;
+            }
+            if ($method2 === 'compileRawEchos') {
+                return 1;
+            }
+
+            if ($method1 === 'compileEscapedEchos') {
+                return -1;
+            }
+            if ($method2 === 'compileEscapedEchos') {
+                return 1;
+            }
+        });
+
+        return $methods;
+    }
+
+    /**
+     * Compile Blade statements that start with "@".
+     *
+     * @param  string  $value
+     * @return mixed
+     */
+    protected function compileStatements($value)
+    {
+        $callback = function ($match) {
+            if (method_exists($this, $method = 'compile'.ucfirst($match[1]))) {
+                $arguments = isset($match[3]) ? $match[3] : $match;
+                $match[0] = $this->$method($arguments);
+            }
+
+            return isset($match[3]) ? $match[0] : $match[0].$match[2];
+        };
+
+        return preg_replace_callback('/\B@(\w+)([ \t]*)(\( ( (?>[^()]+) | (?3) )* \))?/x', $callback, $value);
+    }
+
+    /**
+     * Compile the "raw" echo statements.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function compileRawEchos($value)
+    {
+        $pattern = sprintf('/(@)?%s\s*(.+?)\s*%s(\r?\n)?/s', $this->rawTags[0], $this->rawTags[1]);
+
+        $callback = function ($matches) {
+            $whitespace = empty($matches[3]) ? '' : $matches[3].$matches[3];
+
+            return $matches[1] ? substr($matches[0], 1) : '<?php echo '.$this->compileEchoDefaults($matches[2]).'; ?>'.$whitespace;
+        };
+
+        return preg_replace_callback($pattern, $callback, $value);
+    }
+
+    /**
+     * Compile the "regular" echo statements.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function compileRegularEchos($value)
+    {
+        $pattern = sprintf('/(@)?%s\s*(.+?)\s*%s(\r?\n)?/s', $this->contentTags[0], $this->contentTags[1]);
+
+        $callback = function ($matches) {
+            $whitespace = empty($matches[3]) ? '' : $matches[3].$matches[3];
+
+            $wrapped = sprintf($this->echoFormat, $this->compileEchoDefaults($matches[2]));
+
+            return $matches[1] ? substr($matches[0], 1) : '<?php echo '.$wrapped.'; ?>'.$whitespace;
+        };
+
+        return preg_replace_callback($pattern, $callback, $value);
+    }
+
+    /**
+     * Compile the escaped echo statements.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    protected function compileEscapedEchos($value)
+    {
+        $pattern = sprintf('/(@)?%s\s*(.+?)\s*%s(\r?\n)?/s', $this->escapedTags[0], $this->escapedTags[1]);
+
+        $callback = function ($matches) {
+            $whitespace = empty($matches[3]) ? '' : $matches[3].$matches[3];
+
+            return $matches[1] ? $matches[0] : '<?php echo e('.$this->compileEchoDefaults($matches[2]).'); ?>'.$whitespace;
+        };
+
+        return preg_replace_callback($pattern, $callback, $value);
+    }
+
+    /**
+     * Compile the default values for the echo statement.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function compileEchoDefaults($value)
+    {
+        return preg_replace('/^(?=\$)(.+?)(?:\s+or\s+)(.+?)$/s', 'isset($1) ? $1 : $2', $value);
+    }
+
+    /**
+     * Compile the unless statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileUnless($expression)
+    {
+        return "<?php if ( ! $expression): ?>";
+    }
+
+    /**
+     * Compile the end unless statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileEndunless($expression)
+    {
+        return '<?php endif; ?>';
+    }
+
+    /**
+     * Compile the else statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileElse($expression)
+    {
+        return '<?php else: ?>';
+    }
+
+    /**
+     * Compile the for statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileFor($expression)
+    {
+        return "<?php for{$expression}: ?>";
+    }
+
+    /**
+     * Compile the foreach statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileForeach($expression)
+    {
+        return "<?php foreach{$expression}: ?>";
+    }
+
+    /**
+     * Compile the if statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileIf($expression)
+    {
+        return "<?php if{$expression}: ?>";
+    }
+
+    /**
+     * Compile the else-if statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileElseif($expression)
+    {
+        return "<?php elseif{$expression}: ?>";
+    }
+
+    /**
+     * Compile the while statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileWhile($expression)
+    {
+        return "<?php while{$expression}: ?>";
+    }
+
+    /**
+     * Compile the end-while statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileEndwhile($expression)
+    {
+        return '<?php endwhile; ?>';
+    }
+
+    /**
+     * Compile the end-for statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileEndfor($expression)
+    {
+        return '<?php endfor; ?>';
+    }
+
+    /**
+     * Compile the end-for-each statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileEndforeach($expression)
+    {
+        return '<?php endforeach; ?>';
+    }
+
+    /**
+     * Compile the end-if statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileEndif($expression)
+    {
+        return '<?php endif; ?>';
+    }
+
+    /**
+     * Compile the end-for-else statements into valid PHP.
+     *
+     * @param  string  $expression
+     * @return string
+     */
+    protected function compileEndforelse($expression)
+    {
+        return '<?php endif; ?>';
+    }
+
+    /**
+    * Gets the raw tags used by the compiler.
+    *
+    * @return array
+    */
+    public function getRawTags()
+    {
+        return $this->rawTags;
+    }
+
+    /**
+     * Sets the raw tags used for the compiler.
+     *
+     * @param  string  $openTag
+     * @param  string  $closeTag
+     * @return void
+     */
+    public function setRawTags($openTag, $closeTag)
+    {
+        $this->rawTags = [preg_quote($openTag), preg_quote($closeTag)];
+    }
+
+    /**
+     * Sets the content tags used for the compiler.
+     *
+     * @param  string  $openTag
+     * @param  string  $closeTag
+     * @param  bool    $escaped
+     * @return void
+     */
+    public function setContentTags($openTag, $closeTag, $escaped = false)
+    {
+        $property = ($escaped === true) ? 'escapedTags' : 'contentTags';
+
+        $this->{$property} = [preg_quote($openTag), preg_quote($closeTag)];
+    }
+
+    /**
+     * Sets the escaped content tags used for the compiler.
+     *
+     * @param  string  $openTag
+     * @param  string  $closeTag
+     * @return void
+     */
+    public function setEscapedContentTags($openTag, $closeTag)
+    {
+        $this->setContentTags($openTag, $closeTag, true);
+    }
+
+    /**
+     * Gets the content tags used for the compiler.
+     *
+     * @return string
+     */
+    public function getContentTags()
+    {
+        return $this->getTags();
+    }
+
+    /**
+     * Gets the escaped content tags used for the compiler.
+     *
+     * @return string
+     */
+    public function getEscapedContentTags()
+    {
+        return $this->getTags(true);
+    }
+
+    /**
+     * Gets the tags used for the compiler.
+     *
+     * @param  bool  $escaped
+     * @return array
+     */
+    protected function getTags($escaped = false)
+    {
+        $tags = $escaped ? $this->escapedTags : $this->contentTags;
+
+        return array_map('stripcslashes', $tags);
+    }
+
+    /**
+     * Set the echo format to be used by the compiler.
+     *
+     * @param  string  $format
+     * @return void
+     */
+    public function setEchoFormat($format)
+    {
+        $this->echoFormat = $format;
+    }
+}
