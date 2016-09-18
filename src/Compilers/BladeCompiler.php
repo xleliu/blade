1121b241eca5fbae068593eab397e49d544f3f99
Scholer <scholer_l@live.com>
2016-09-18 22:27:02 +0800

    remove bracket

--- src/Compilers/BladeCompiler.php
+++ src/Compilers/BladeCompiler.php
@@ -243,7 +243,7 @@ class BladeCompiler extends Compiler implements CompilerInterface
             if (strpos($match[1], '@') !== false) {
                 $match[0] = isset($match[3]) ? $match[1].$match[3] : $match[1];
             } elseif (isset($this->customDirectives[$match[1]])) {
-                $match[0] = call_user_func($this->customDirectives[$match[1]], (array_key_exists(3, $match) ? $match[3] : ''));
+                $match[0] = call_user_func($this->customDirectives[$match[1]], array_key_exists(3, $match) ? $match[3] : '');
             } elseif (method_exists($this, $method = 'compile'.ucfirst($match[1]))) {
                 $match[0] = $this->$method(array_key_exists(3, $match) ? $match[3] : '');
             }
