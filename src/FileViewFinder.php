3718bdcb7785980c3694d30eda4199b629ed7132
Terran <wuchaoboy@gmail.com>
2017-02-03 20:27:18 +0800

    component & slot 支持

--- src/FileViewFinder.php
+++ src/FileViewFinder.php
@@ -126,7 +126,7 @@ class FileViewFinder implements ViewFinderInterface
     {
         foreach ((array) $paths as $path) {
             foreach ($this->getPossibleViewFiles($name) as $file) {
-                if ($this->files->exists($viewPath = $path.'/'.$file)) {
+                if ($this->files->exists($viewPath = $path . '/' . $file)) {
                     return $viewPath;
                 }
             }
